9ff8dced4f206f3d4d3234701566c1d68c540135
mbish08 <mdrew8@gmail.com>
2020-09-30 16:27:27 -0700

    first commit

---
+++ spec/first/project_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe First::Project do
+  it "has a version number" do
+    expect(First::Project::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
